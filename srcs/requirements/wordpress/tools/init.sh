5b8c18ec58c6308e41c69e256021fad4a54a8b84
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-04-20 15:10:30 +0100

    fix: Corrigir nomes de variáveis na conexão MySQL e melhorar a formatação condicional

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -2,15 +2,16 @@
 
 set -e
 
-if     [ -f /run/secrets/db_password ] 
-    && [ -f /run/secrets/wp_admin_password ] 
-    && [ -f /run/secrets/wp_editor_password ]; then
+if [ -f /run/secrets/db_password ] && \
+   [ -f /run/secrets/wp_admin_password ] && \
+   [ -f /run/secrets/wp_editor_password ]; then
+
     DB_PASSWORD="$(cat /run/secrets/db_password)"
     WP_ADMIN_PASSWORD="$(cat /run/secrets/wp_admin_password)"
     WP_EDITOR_PASSWORD="$(cat /run/secrets/wp_editor_password)"
 fi
 
-until mysql -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" -e "SELECT 1" > /dev/null 2>&1; do
+until mysql -h"$DB_HOST" -u"$MYSQL_USER" -p"$DB_PASSWORD" -e "SELECT 1" > /dev/null 2>&1; do
     sleep 2
 done
 
